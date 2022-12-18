075a91b6792c235888cc507feaf57d20471018ee
Byun Jeongyong <byunjy@markany.com>
2022-12-19 00:02:59 +0900

    first commit

---
+++ a.java
@@ -0,0 +1,10 @@
+package a;
+public class A {
+    public void printA(){
+        System.out.println("A is A");
+    }
+    public static void main(String[] args){
+        A a = new A();
+        a.printA();
+    }
+}
