ee044142fa6f8708cbe75b1df466e06bc2baf621
byunjy2022 <byunjy@markany.com>
2023-02-18 13:07:28 +0900

    2nd commit

--- a.java
+++ a.java
@@ -2,6 +2,7 @@ package a;
 public class A {
     public void printA(){
         System.out.println("A is A");
+        System.out.println("B is B");
     }
     public static void main(String[] args){
         A a = new A();
